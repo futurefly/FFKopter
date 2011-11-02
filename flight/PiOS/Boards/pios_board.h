c59aac8f835d4de79793fca22f8dd3f8669c0808
Patrik GONDA <shredder@phenom.gondovci>
2011-11-02 22:57:52 +0100

    commit comp

--- flight/PiOS/Boards/pios_board.h
+++ flight/PiOS/Boards/pios_board.h
@@ -1,16 +1,8 @@
 #ifndef PIOS_BOARD_H_
 #define PIOS_BOARD_H_
 
-#ifdef USE_STM32103CB_AHRS
-#include "STM32103CB_AHRS.h"
-#elif USE_STM3210E_OP
-#include "STM3210E_OP.h"
-#elif USE_STM32103CB_PIPXTREME
-#include "STM32103CB_PIPXTREME_Rev1.h"
-#elif USE_STM32103CB_CC_Rev1
-#include "STM32103CB_CC_Rev1.h"
-#elif USE_STM3210E_INS
-#include "STM3210E_INS.h"
+#ifdef USE_LPCXPresso
+#include "LPCXPresso.h"
 #else
 #error Board definition has not been provided.
 #endif
